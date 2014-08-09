2c83510e0a09fdbf816c584daf3c878730424874
Sergey Danielyan <sdanielyan@ebosphorus.com>
2014-08-09 12:53:59 +1100

    Add C# wrapper for the library

---
+++ Managed/ServiceInstallerManaged/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows Services Runner")]
+[assembly: AssemblyDescription("Wrapper for Native library to work with Windows Services")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceInstallerManaged")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5572ee49-d448-4c68-b350-b2ab392c062a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
---
+++ Managed/ServiceInstallerManaged/SrvInstallerManaged.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace WinSrvRunnerManaged
+{
+    // According to 
+    // http://msdn.microsoft.com/en-us/library/windows/desktop/ms685996(v=vs.85).aspx
+    public enum WinSrvState
+    {
+        ServiceStopped          = 1,
+        ServiceStartPending     = 2,
+        ServiceStopPending      = 3,
+        ServiceRunning          = 4,
+        ServiceContinuePending  = 5,
+        ServicePausePending     = 6,
+        ServicePaused           = 7
+    }
+
+    public static class SrvInstallerManaged
+    {
+        #region Error codes
+
+        public const Int32 InstallerOpSuccess                   = 0;
+        public const Int32 InstallerErrorOpenScManager          = 0x100;
+        public const Int32 InstallerErrorCreateService          = 0x101;
+        public const Int32 InstallerErrorOpenService            = 0x102;
+        public const Int32 InstallerErrorQueryService           = 0x103;
+        public const Int32 InstallerErrorStartService           = 0x104;
+        public const Int32 InstallerErrorStopService            = 0x105;
+        public const Int32 InstallerErrorPauseService           = 0x106;
+        public const Int32 InstallerErrorContinueService        = 0x107;
+        public const Int32 InstallerErrorInvalidState           = 0x108;
+        public const Int32 InstallerErrorTransitionFailure      = 0x109;
+        public const Int32 InstallerErrorWrongAction            = 0x110;
+        public const Int32 InstallerErrorInvalidHandle          = 0x111;
+        public const Int32 InstallerErrorAccessDenied           = 0x112;
+        public const Int32 InstallerErrorDeleteService          = 0x113;
+        public const Int32 InstallerErrorInvalidName            = 0x114;
+        public const Int32 InstallerErrorServiceDoesNotExist    = 0x115;
+
+        #endregion
+
+        #region Native functions
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceQueryStateByName([In] String srvName, [Out] out Int32 scState);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool ServiceExists([In] String srvName);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceInstall([In] String srvName, [In] String srvFullName, [In] String srvPath);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceUninstall([In] String srvName);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceStart([In] String srvName);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceStop([In] String srvName);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServicePause([In] String srvName);
+
+        [DllImport("WinSrvRunner.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern Int32 ServiceResume([In] String srvName);
+
+        #endregion
+
+        #region Wrapper functions
+
+        public static WinSrvState GetServiceStateByName(String srvName)
+        {
+            if (String.IsNullOrEmpty(srvName))
+                throw new ArgumentNullException("srvName");
+
+            int scState;
+            int win32Error = ServiceQueryStateByName(srvName, out scState);
+
+            if (win32Error != 0)
+                throw new Win32Exception(win32Error, "WinSrvRunner error");
+
+            return (WinSrvState) Enum.ToObject(typeof (WinSrvState), scState);
+        }
+
+        #endregion
+
+    }
+}
